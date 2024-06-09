724d17788fa5a5702a06c2f6b4bdc64e966c389d
zihad25322 <162394310+zihad25322@users.noreply.github.com>
2024-06-09 15:16:45 +0600

    Create npx8.js

---
+++ scripts/commands/npx8.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+module.exports = {
+  config:{
+	name: "😒",
+        version: "1.0.1",
+        prefix: false,
+	permssion: 0,
+	credits: "nayan", 
+	description: "Fun",
+	category: "no prefix",
+	usages: "😒",
+        cooldowns: 5, 
+},
+
+handleEvent: function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+  const content = event.body ? event.body : '';
+  const body = content.toLowerCase();
+	if (body.indexOf("😅")==0 || body.indexOf("😊")==0 || body.indexOf("🤗")==0 || body.indexOf("😌")==0) {
+		var msg = {
+				body: "বাবু 🫠🙃",
+				attachment: fs.createReadStream(__dirname + `/Nayan/Mayabi.mp3`)
+			}
+			api.sendMessage( msg, threadID, messageID);
+    api.setMessageReaction("🫗", event.messageID, (err) => {}, true)
+		}
+	},
+	start: function({ nayan }) {
+
+  }
+}
