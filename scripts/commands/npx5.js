9b87450c0827ac248f65a18fe456d5ab6d507717
zihad25322 <162394310+zihad25322@users.noreply.github.com>
2024-06-09 21:26:27 +0600

    Update npx5.js

--- scripts/commands/npx5.js
+++ scripts/commands/npx5.js
@@ -19,7 +19,7 @@ handleEvent: function({ api, event, client, __GLOBAL }) {
 	if (body.indexOf("🙄")==0 || body.indexOf("😼")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0) {
 		var msg = {
 				body: "আমি আই দিকে সোনা 🫠",
-				attachment: fs.createReadStream(__dirname + `/Nayan/upray.mp3`)
+				attachment: fs.createReadStream(__dirname + `/cache/upray.mp3`)
 			}
 			api.sendMessage( msg, threadID, messageID);
     api.setMessageReaction("😫", event.messageID, (err) => {}, true)
