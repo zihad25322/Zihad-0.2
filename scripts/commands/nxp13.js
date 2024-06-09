0bfd383221638118223b54a4f50de226f54f60b0
zihad25322 <162394310+zihad25322@users.noreply.github.com>
2024-06-09 15:35:47 +0600

    Create nxp13.js

---
+++ scripts/commands/nxp13.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+module.exports = {
+  config:{
+	name: "😒",
+        version: "1.0.1",
+        prefix: false,
+	permssion: 0,
+	credits: "nayan", 
+	description: "Fun",
+	category: "no prefix",
+	usages: "😒",
+        cooldowns: 5, 
+},
+
+handleEvent: function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+  const content = event.body ? event.body : '';
+  const body = content.toLowerCase();
+	if (body.indexOf("🥵")==0 || body.indexOf("😫")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0) {
+		var msg = {
+				body: "বাবু ছাড়ো নাহ এখন। আর কতো? ব্যাথা লাগছে তো।.!! 😾🥀🖤",
+				attachment: fs.createReadStream(__dirname + `/Nayan/betha.mp3`)
+			}
+			api.sendMessage( msg, threadID, messageID);
+    api.setMessageReaction("👅", event.messageID, (err) => {}, true)
+		}
+	},
+	start: function({ nayan }) {
+
+  }
+}
