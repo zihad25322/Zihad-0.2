1987f8e5499514fc9c388b4247a35a6819bd0161
zihad25322 <162394310+zihad25322@users.noreply.github.com>
2024-06-09 21:48:02 +0600

    Update nxp13.js

--- scripts/commands/nxp13.js
+++ scripts/commands/nxp13.js
@@ -1,14 +1,14 @@
 const fs = require("fs");
 module.exports = {
   config:{
-	name: "😒",
+	name: "🥵",
         version: "1.0.1",
         prefix: false,
 	permssion: 0,
 	credits: "nayan", 
 	description: "Fun",
 	category: "no prefix",
-	usages: "😒",
+	usages: "🥵",
         cooldowns: 5, 
 },
 
@@ -16,7 +16,7 @@ handleEvent: function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
-	if (body.indexOf("🥵")==0 || body.indexOf("😫")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0) {
+	if (body.indexOf("🥵")==0 || body.indexOf("😫")==0 || body.indexOf("🍆")==0 || body.indexOf("🍌")==0) {
 		var msg = {
 				body: "বাবু ছাড়ো নাহ এখন। আর কতো? ব্যাথা লাগছে তো।.!! 😾🥀🖤",
 				attachment: fs.createReadStream(__dirname + `/Nayan/betha.mp3`)
